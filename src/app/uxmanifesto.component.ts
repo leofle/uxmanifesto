e3641c8718e919ed556d1cec01098acb4f4bafb3
leofle <leofle@gmail.com>
2016-07-27 18:08:42 +0300

    refactoring

--- src/app/uxmanifesto.component.ts
+++ src/app/uxmanifesto.component.ts
@@ -36,14 +36,16 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 export class UxmanifestoAppComponent {
 
-  title = 'UXmanifesto';
-  subtitle = 'What is Experience for us';
-  paragraph = `Experience is the combination of perceptions, emotions, memories, responses and knowledge that result from an interaction, actual or expected, with a product, a system, a service or a brand.It concerns everything that a person feels before (expectation), during(the proper experience) and after (memories) this interaction.
-  We are in the Customer Age.Today people want enriched experiences that improve their lives.Customers reward companies whose products exceed their expectations.
-  Brands that address this shift by building their business around a brand of listening and working with customers become and stay profitable over time.`
-
+  title:      string;
+  subtitle:   string;
+  paragraph:  string;
 
   constructor() {
+    this.title = 'UXmanifesto';
+    this.subtitle = 'What is Experience for us';
+    this.paragraph = `Experience is the combination of perceptions, emotions, memories, responses and knowledge that result from an interaction, actual or expected, with a product, a system, a service or a brand.It concerns everything that a person feels before (expectation), during(the proper experience) and after (memories) this interaction.
+    We are in the Customer Age.Today people want enriched experiences that improve their lives.Customers reward companies whose products exceed their expectations.
+    Brands that address this shift by building their business around a brand of listening and working with customers become and stay profitable over time.`
 
   }
 
