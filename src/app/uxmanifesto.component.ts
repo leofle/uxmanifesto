b547659e573d6b2cfe9efa127c9938cf5644fa0f
leofle <leofle@gmail.com>
2016-07-27 14:43:45 +0300

    installing sass

--- src/app/uxmanifesto.component.ts
+++ src/app/uxmanifesto.component.ts
@@ -22,10 +22,10 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
       <p>{{paragraph}}</p>
 
     <div class="navBtns">
-      <a [routerLink]="['/route1']" class="btnPrime" (click)="clicked($event)">What</a>
-      <a [routerLink]="['/route2']" class="btnPrime" (click)="clicked($event)">Why</a>
-      <a [routerLink]="['/route3']" class="btnPrime" (click)="clicked($event)">How</a>
-      <a [routerLink]="['/route4']" class="btnPrime" (click)="clicked($event)">Get</a>
+      <a [routerLink]="['/route1']" routerLinkActive="router-link" class="btnPrime" (click)="clicked($event)">What</a>
+      <a [routerLink]="['/route2']" routerLinkActive="router-link" class="btnPrime" (click)="clicked($event)">Why</a>
+      <a [routerLink]="['/route3']" routerLinkActive="router-link" class="btnPrime" (click)="clicked($event)">How</a>
+      <a [routerLink]="['/route4']" routerLinkActive="router-link" class="btnPrime" (click)="clicked($event)">Get</a>
     </div>
     <router-outlet></router-outlet>
     </div>
@@ -35,13 +35,17 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 })
 
 export class UxmanifestoAppComponent {
-	constructor() { }
 
   title = 'UXmanifesto';
   subtitle = 'What is Experience for us';
   paragraph = `Experience is the combination of perceptions, emotions, memories, responses and knowledge that result from an interaction, actual or expected, with a product, a system, a service or a brand.It concerns everything that a person feels before (expectation), during(the proper experience) and after (memories) this interaction.
   We are in the Customer Age.Today people want enriched experiences that improve their lives.Customers reward companies whose products exceed their expectations.
   Brands that address this shift by building their business around a brand of listening and working with customers become and stay profitable over time.`
+
+
+  constructor() {
+
+  }
 
   clicked(event) {
     console.log( `Hi you click the button ${ event.target.innerHTML } , congrats!` );
